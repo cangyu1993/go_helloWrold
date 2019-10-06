e5ee12ac7527e8771afe0a984ecc5f0a0f327711
cangyu1993 <1765244720@qq.com>
2019-10-06 16:16:44 +0800

    整合ide

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	var a = 100
-	var b = 200
-	var c = a + b
-
-	fmt.Println(c)
-	duration := time.Duration(10) * time.Second
-	time.Sleep(duration)
+  fmt.Printf("7777")
 }
